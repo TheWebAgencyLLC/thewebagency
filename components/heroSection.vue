39f75103f8323024497f203d88eb3e1e1b49e05d
Raph <raphy@raphsant.com>
2025-01-30 15:13:54 -0600

    Add hero section component
    Introduce a new HeroSection component with a styled headline and call-to-action buttons. The design includes gradient-styled buttons and attention-grabbing text for improved user engagement.

---
+++ components/heroSection.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="text-white text-7xl">
+    &lt;All your web needs, <span class="text-[#E70D01]">in</span> <span class="text-[#FF5400]">one</span> <span
+      class="text-[#F77D05]">place/&gt;</span>
+  </div>
+  <div class="flex justify-center items-center text-white gap-8 text-xl font-light ">
+    <div class="flex space-x-4">
+      <!-- Button 1 -->
+      <div class="bg-gradient-to-r from-[#E70D01] via-[#FF5400] to-[#F77D05] p-[2px] rounded-md">
+        <button class="px-4 py-2   text-gray-100 font-light rounded-md">
+          Learn More
+        </button>
+      </div>
+
+      <!-- Button 2 -->
+      <div class="bg-gradient-to-r from-[#E70D01] via-[#FF5400] to-[#F77D05] p-[2px] rounded-md">
+        <button class="px-4 py-2 bg-black text-white font-semibold rounded-md">
+          Portfolio
+        </button>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
