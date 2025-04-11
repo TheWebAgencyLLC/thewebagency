49e758ee1df6f25ec3d37ad9f0db366c5d8969cd
Sneh <sneh.mehta23@gmail.com>
2025-04-10 21:03:59 -0500

    feat: added cal integration back

--- pages/index.vue
+++ pages/index.vue
@@ -611,6 +611,55 @@ onMounted(() => {
   if (bigIdeasSectionRef.value) {
     observer.observe(bigIdeasSectionRef.value);
   }
+  // Cal.com initialization script
+function initCalCom() {
+    (function (C, A, L) {
+      let p = function (a, ar) {
+        a.q.push(ar);
+      };
+      let d = C.document;
+      C.Cal =
+        C.Cal ||
+        function () {
+          let cal = C.Cal;
+          let ar = arguments;
+          if (!cal.loaded) {
+            cal.ns = {};
+            cal.q = cal.q || [];
+            d.head.appendChild(d.createElement("script")).src = A;
+            cal.loaded = true;
+          }
+          if (ar[0] === L) {
+            const api = function () {
+              p(api, arguments);
+            };
+            const namespace = ar[1];
+            api.q = api.q || [];
+            if (typeof namespace === "string") {
+              cal.ns[namespace] = cal.ns[namespace] || api;
+              p(cal.ns[namespace], ar);
+              p(cal, ["initNamespace", namespace]);
+            } else p(cal, ar);
+            return;
+          }
+          p(cal, ar);
+        };
+    })(window, "https://app.cal.com/embed/embed.js", "init");
+
+    Cal("init", "discovery", { origin: "https://cal.com" });
+    Cal.ns.discovery("inline", {
+      elementOrSelector: "#my-cal-inline",
+      config: { layout: "month_view" },
+      calLink: "the-web-agency/discovery",
+    });
+    Cal.ns.discovery("ui", {
+      hideEventTypeDetails: false,
+      layout: "month_view",
+    });
+  }
+
+  // Call the initialization function
+  initCalCom();
 })
 
 const capabilities = [
